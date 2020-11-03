cc097215822969f1ad5f7a8a168a66d744ad1d67
seungh0 <will.seungho@gmail.com>
2020-11-03 18:44:50 +0900

    게시물 페이지

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import MarketLine from 'containers/MarketLine';
 import NotFound from 'containers/NotFound';
+import MarketProduct from 'containers/MarketProduct';
 
 const Routes = () => (
   <Switch>
+    <Route exact path="/product" component={MarketProduct} />
     <Route exact path="/" component={MarketLine} />
     <Route exact path="/404" component={NotFound} />
   </Switch>
