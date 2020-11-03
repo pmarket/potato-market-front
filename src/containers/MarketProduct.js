3662b84e24f902aa618157e6021d913f0200d9ae
seungh0 <will.seungho@gmail.com>
2020-11-03 18:44:50 +0900

    게시물 페이지

---
+++ src/containers/MarketProduct.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/**
+ * /potato-market-front/product 페이지
+ */
+const MarketProduct = () => {
+  return (
+    <div>
+      <h1>Product</h1>
+    </div>
+  );
+};
+
+export default MarketProduct;
