42f3ef0a2936863ee7a1a746694bfd889435d980
seungh0 <will.seungho@gmail.com>
2020-11-05 15:31:00 +0900

    github action 시크릿 설정 파일 추가

--- src/containers/auth/Login.js
+++ src/containers/auth/Login.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import GOOGLE_OAUTH_URL from 'constants/GoogleOAuth';
+import axios from 'axios';
+
+const { REACT_APP_API_URI } = process.env;
 
 const Login = () => {
+  useEffect(() => {
+    axios
+      .get(`${REACT_APP_API_URI}/ping`)
+      .then((response) => {
+        console.log('서버 연결 완료');
+      })
+      .catch(() => {
+        alert('서버와 연결되어 있지 않습니다');
+      });
+  }, []);
   return (
     <div>
       <Button variant="contained" href={GOOGLE_OAUTH_URL}>
