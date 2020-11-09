caaab8564b3051399a74f1eff8a982894a8ceaa0
이정원 <wjddnjswjd12@gmail.com>
2020-11-09 18:35:38 +0900

    jwl's 2nd commit

--- src/containers/BoardWrite.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import CKEditor from 'ckeditor4-reart';
-import { Button, Form } from "react-bootstrap";
--- src/containers/MarketProduct.js
+++ src/containers/MarketProduct.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Navigation from './Nav.js';
-import gamza from '../images/hansei.jpg'
+import gamza from '../images/hansei.jpg';
 
-/**
- * /potato-market-front/product 페이지
- */
+
+/* 
+potato-market-front/product 페이지
+*/
+
 const MarketProduct = () => {
   return (
     <div>
       <Navigation />
 
-      <img class="bg" src={gamza} />
+      <img class="bg" src={gamza} alt="" />
     </div>
   );
 };
