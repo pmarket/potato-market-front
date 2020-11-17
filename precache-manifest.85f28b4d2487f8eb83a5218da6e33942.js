f4e9f11db11026954f1087b1a47affd6e8e36e54
seungh0 <will.seungho@gmail.com>
2020-11-17 05:14:47 +0000

    Deploying to deploy from  @ 04cf02f2f9bb4944ea040e383bf504c52f999069 🚀

--- precache-manifest.85f28b4d2487f8eb83a5218da6e33942.js
+++ precache-manifest.85f28b4d2487f8eb83a5218da6e33942.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bdc6d902ef0ffa49f9f630c3eab5fa8b",
+    "revision": "e66e9b6a93ffe0c416e798b5ef031ae6",
     "url": "/potato-market-front/index.html"
   },
   {
-    "revision": "e926e3994bfe0e373120",
+    "revision": "5c1c4127d1e35490ceed",
     "url": "/potato-market-front/static/css/main.6ab09713.chunk.css"
   },
   {
-    "revision": "8b31ead7ce164afee72f",
-    "url": "/potato-market-front/static/js/2.6fa665d0.chunk.js"
+    "revision": "b191e6742280c4bf8f1a",
+    "url": "/potato-market-front/static/js/2.8d922030.chunk.js"
   },
   {
     "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
-    "url": "/potato-market-front/static/js/2.6fa665d0.chunk.js.LICENSE.txt"
+    "url": "/potato-market-front/static/js/2.8d922030.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e926e3994bfe0e373120",
-    "url": "/potato-market-front/static/js/main.dde277d9.chunk.js"
+    "revision": "5c1c4127d1e35490ceed",
+    "url": "/potato-market-front/static/js/main.3c5e0635.chunk.js"
   },
   {
     "revision": "f069b6a259dd7575afcc",
