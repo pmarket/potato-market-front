64e99d986e6c655ab16e0702b6c2a94143cea8ca
wjddnjswjd12 <wjddnjswjd12@gmail.com>
2020-11-17 13:20:25 +0900

    body 말고 container 에 gamza1 띄우기

--- src/containers/MarketProduct.js
+++ src/containers/MarketProduct.js
@@ -1,6 +1,5 @@
 import ButtonSignUp from 'elements/ButtonSignUp';
 import React from 'react';
-import gamza from 'assets/images/main.jpg';
 import './MarketProduct.css';
 
 const MarketProduct = () => {
