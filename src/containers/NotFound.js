50a670d5860594998fa8e662c6d5d7fc0ac1f500
kect3578 <kect3578@gmail.com>
2020-11-18 18:40:50 +0900

    404페이지은택이가바꿔보기

--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Alert, AlertTitle } from '@material-ui/lab';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-    '& > * + *': {
-      marginTop: theme.spacing(2),
-    },
-  },
-}));
+import gamza from 'assets/images/gamza.jpg';
 
 const NotFound = () => {
-  const classes = useStyles();
   return (
-    <div className={classes.root}>
-      <Alert severity="error">
-        <AlertTitle>404 Error</AlertTitle>
-        Not Found
-      </Alert>
+    <div className="App">
+      404 not pages !
+      <img src={gamza} />
     </div>
+
   );
 };
 
