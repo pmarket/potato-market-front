376dbc3ec739f596ae1438e87d8e70b13bf4e436
will0212 <74108116+will0212@users.noreply.github.com>
2020-11-20 13:32:40 +0000

    Deploying to deploy from  @ 6a403f1ea0a635e5aecf18f2463e723e810b34df 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/potato-market-front/precache-manifest.2e7c1e2763e4a76f1ee0e42a7083f49c.js"
+  "/potato-market-front/precache-manifest.de65e3fe68364dde574266d64b7fe44c.js"
 );
 
 self.addEventListener('message', (event) => {
