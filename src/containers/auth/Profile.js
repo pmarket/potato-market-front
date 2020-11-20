63879e16607e662f13915cb0acb1f5c4685a2b3f
will0212 <test.seungho@gmail.com>
2020-11-21 00:11:15 +0900

    feat: add Profile Update

--- src/containers/auth/Profile.js
+++ src/containers/auth/Profile.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
-import { makeStyles, Grid, Avatar, Button } from '@material-ui/core';
+import { makeStyles, Grid, Avatar, Button, TextField } from '@material-ui/core';
 import Paper from 'elements/Paper';
 
 const { REACT_APP_API_URI } = process.env;
@@ -13,12 +13,24 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(0, 3),
     marginTop: '10%',
     height: '100%',
+    '& .MuiTextField-root': {
+      margin: '3%',
+      marginLeft: '10%',
+      marginRight: '10%',
+      width: '80%',
+    },
   },
   center: {
     textAlign: 'center',
   },
   image: {
     marginLeft: '45%',
+  },
+  button: {
+    width: '80%',
+    marginLeft: '10%',
+    marginRight: '10%',
+    marginTop: '8%',
   },
 }));
 
@@ -54,6 +66,40 @@ const Profile = () => {
     history.push('/');
   };
 
+  const nameOnChange = (e) => {
+    setProfile({
+      id: profile.id,
+      email: profile.email,
+      profileUrl: profile.profileUrl,
+      provider: profile.provider,
+      name: e.target.value,
+    });
+  };
+
+  const handleUpdateProfile = () => {
+    const token = localStorage.getItem('token');
+    axios
+      .put(
+        `${REACT_APP_API_URI}/api/v1/member`,
+        {
+          name: profile.name,
+          profileUrl: profile.profileUrl,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        },
+      )
+      .then(() => {
+        alert('수정되었습니다');
+      })
+      .catch((error) => {
+        alert(error.response.data.message);
+      });
+  };
+
   return (
     <div className={classes.root}>
       <Paper variant="outlined">
@@ -68,9 +114,34 @@ const Profile = () => {
               className={classes.image}
             />
           </Grid>
-          <h4 className={classes.center}>{profile.email}</h4>
-          <h4 className={classes.center}>{profile.name}</h4>
-          <Button variant="contained" color="primary" onClick={handleLogout}>
+          <TextField
+            label="Email"
+            multiline
+            rowsMax={2}
+            value={profile.email}
+            disabled
+          />
+          <TextField
+            label="Name"
+            multiline
+            rowsMax={2}
+            value={profile.name}
+            onChange={nameOnChange}
+          />
+          <Button
+            className={classes.button}
+            variant="contained"
+            color="primary"
+            onClick={handleUpdateProfile}
+          >
+            프로필 수정하기
+          </Button>
+          <Button
+            className={classes.button}
+            variant="contained"
+            color="primary"
+            onClick={handleLogout}
+          >
             로그아웃 하기
           </Button>
         </Grid>
