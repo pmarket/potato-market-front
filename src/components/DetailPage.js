234856797606c86a829c465a2afa85eba14d7d58
Jungwon Lee <leejungwon96@gmail.com>
2020-11-25 20:12:30 +0900

    showing sender info

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './DetailPage.css';
-import gamza from '../assets/images/logingamza.jpg';
 
 const { REACT_APP_API_URI } = process.env;
 
 const DetailPage = () => {
   const [product, setProduct] = useState([]);
+  const [sender, setSender] = useState([]);
 
   useEffect(() => {
     const productId = window.location.href.split('detailpage/')[1];
@@ -14,6 +14,7 @@ const DetailPage = () => {
       .get(`${REACT_APP_API_URI}/api/v1/product?productId=${productId}`)
       .then((response) => {
         setProduct(response.data.data.product);
+        setSender(response.data.data.sender);
       });
   }, []);
 
@@ -21,16 +22,16 @@ const DetailPage = () => {
     <div className="detail-container">
       <div className="detail-container-top">
         <div className="dct-left">
-          <img src={gamza} alt="wtf" />
+          <img src={product.profileUrl} alt="" />
         </div>
         <div className="dct-right">
           <h1>제목:{product.name}</h1>
-          <h4>판매자:</h4>
+          <h4>판매자:{sender.name}</h4>
         </div>
       </div>
       <div>
-        <h4>판매자이메일:</h4>
-        <h4>내용:</h4>
+        <h4>판매자이메일:{sender.email}</h4>
+        <h4>내용:{product.content}</h4>
       </div>
     </div>
   );
